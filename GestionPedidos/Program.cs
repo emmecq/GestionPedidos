1ebffcf26ad6cef54fc7a8aaa193404e8616d91c
emmecq <emmanuelecheverriq@gmail.com>
2026-02-24 09:52:32 -0500

    Archivos base de proyecto

---
+++ GestionPedidos/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        decimal montoPedido;
+        int cantidadItems;
+        string ciudadDestino, tipoCliente, categoriaDespacho;
+    }
+}
