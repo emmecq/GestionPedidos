fb1a957f27f9d846085b7fd4e6b3073055c12ced
emmecq <emmanuelecheverriq@gmail.com>
2026-02-26 09:51:43 -0500

    Se establecieron algunas variables, y se añadieron ciclos que evalúan la entrada de las mismas

--- GestionPedidos/Program.cs
+++ GestionPedidos/Program.cs
@@ -4,8 +4,37 @@ public class Program
 {
     public static void Main()
     {
-        decimal montoPedido;
+        decimal montoPedido, costoEnvio;
         int cantidadItems;
-        string ciudadDestino, tipoCliente, categoriaDespacho;
+        string entradaMontoPedido, tipoCliente, entradaCantidadItems;// ciudadDestino, categoriaDespacho;
+
+        Console.WriteLine("Ingrese el monto del pedido:");
+        entradaMontoPedido = Console.ReadLine();
+        while(!decimal.TryParse(entradaMontoPedido, out montoPedido))
+        {
+            Console.WriteLine("La entrada ingresada no es un número. \nIngrese el monto del pedido:");
+            entradaMontoPedido = Console.ReadLine();
+        }
+        Console.WriteLine("El monto ingresado fue:" + montoPedido);
+
+
+        Console.WriteLine("Ingrese el tipo de cliente: ");
+        tipoCliente = Console.ReadLine()??"";
+        while(tipoCliente != "nuevo" && tipoCliente != "recurrente")
+        {
+            Console.WriteLine("El tipo de cliente no es válido. \nIngrese el tipo de cliente:");
+            tipoCliente = Console.ReadLine()??"";
+        }
+
+
+        Console.WriteLine("Ingrese la cantidad de items:");
+        entradaCantidadItems = Console.ReadLine();
+        while(!int.TryParse(entradaCantidadItems, out cantidadItems))
+        {
+            Console.WriteLine("La entrada ingresada no es un número. \nIngrese la cantidad de items:");
+            entradaCantidadItems= Console.ReadLine();
+        }
+
+
     }
 }
