08501206b4f2ce873301db4a81ee77910e352993
emmecq <emmanuelecheverriq@gmail.com>
2026-05-05 08:54:18 -0500

    Iniciado taller 3 Cambiados nombres de variables para mayor claridad

---
+++ Taller3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class AplicacionNomina
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 3; i++)
+        {
+            // Se cambian los siguientes nombres de variables para mayor claridad:
+            // h por horas
+            // t por tarifa
+            // b por salarioBruto (horas * tarifa)
+            // p por salarioNeto
+
+            Console.Write($"Empleado {i} horas: ");
+            double horas = double.Parse(Console.ReadLine()); 
+            Console.Write($"Empleado {i} tarifa: ");
+            double tarifa = double.Parse(Console.ReadLine());
+            double salarioBruto = horas * tarifa;
+            double salarioNeto = b * 0.92;
+            Console.WriteLine($"Bruto {salarioBruto:C} Neto {salarioNeto:C}");
+        }
+    }
+
+}
