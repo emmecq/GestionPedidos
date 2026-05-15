0905cf963df568a47a008320f6a135e2b5455634
emmecq <emmanuelecheverriq@gmail.com>
2026-05-14 21:54:01 -0500

    feat: agregar función LeerCantidadClientes

---
+++ Gimnasio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program{
+
+	// <summary>
+	// Solicita al usuario la cantidad de clientes a ingresar, asegurando que sea un número entero positivo.
+	// </summary>
+	// <returns>
+	// Un número entero mayor o igual a 1 que representa la cantidad de clientes.
+	// </returns> 
+	static int LeerCantidadClientes()
+	{
+		int cantidadClientes;
+		Console.WriteLine("Cantidad de clientes a ingresar:");
+		while(!int.TryParse(Console.ReadLine(), out cantidadClientes) || cantidadClientes < 1)
+		{
+			Console.WriteLine("Entrada inválida. Favor reingresar:");
+		}
+
+		return cantidadClientes;
+	}	
+	
+}
